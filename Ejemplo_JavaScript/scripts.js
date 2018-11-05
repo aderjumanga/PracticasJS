51e4ea018c408e1ea81b7add4f3e061e2d33be20
aderjumanga <aderjumanga@upeu.edu.pe>
2018-11-04 22:14:30 -0500

    PrimerComitMesage

---
+++ Ejemplo_JavaScript/scripts.js
@@ -0,0 +1,17 @@
+// Quitar esta alert en producion.
+//alert("Estoy Programando");
+
+//---Esto es un objeto Literal----
+function saludar(){
+alert ("Buenos Días");
+}
+
+//---- Esto es una funcion con expresiones----
+var miObjeto = {
+    nombre: "telefono"
+};
+
+var despedirse = function(){
+    console.log("Adios");
+};
+
